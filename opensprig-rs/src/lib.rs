ce1bb7d84903b94f7182a9884fda08947b485b9a
meeplabsdev <76091175+meeplabsdev@users.noreply.github.com>
2026-03-18 19:29:23 +0000

    misc: Slight cleanup and organisation

--- opensprig-rs/src/lib.rs
+++ opensprig-rs/src/lib.rs
@@ -1,31 +1,11 @@
 #![no_std]
 #![no_main]
 
+pub mod hardware;
+
 use defmt_rtt as _;
 use embassy_rp as _;
 use panic_probe as _;
-
-// #[macro_export]
-// macro_rules! aligned_flash {
-//     ($addr:expr, $size:expr) => {{
-//         {
-//             static BYTES: &[u8; $size] = include_flash!($addr, $size);
-//             static ALIGNED_BYTES: &cyw43::Aligned<cyw43::A4, [u8; $size]> = &cyw43::Aligned(*BYTES);
-//             ALIGNED_BYTES
-//         }
-//     }};
-// }
-
-// #[macro_export]
-// macro_rules! include_flash {
-//     ($addr:expr, $size:expr) => {{
-//         {
-//             static BYTES: &[u8] = unsafe { core::slice::from_raw_parts($addr as *const u8, $size) };
-//             static SIZED_BYTES: &[u8; $size] = unsafe { &*(BYTES.as_ptr() as *const [u8; $size]) };
-//             SIZED_BYTES
-//         }
-//     }};
-// }
 
 #[macro_export]
 macro_rules! aligned_flash {
