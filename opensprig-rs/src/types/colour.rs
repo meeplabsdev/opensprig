b1e6c416ba91e8226a4791b8cb12b75a857db204
meeplabsdev <76091175+meeplabsdev@users.noreply.github.com>
2026-03-25 01:36:09 +0000

    feat: Add class for represeting colours

---
+++ opensprig-rs/src/types/colour.rs
@@ -0,0 +1,23 @@
+pub struct Colour {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl Colour {
+    pub fn new(r: u8, g: u8, b: u8) -> Self {
+        Self { r, g, b }
+    }
+
+    pub fn as_16bit(&self) -> u16 {
+        rgb(self.r, self.g, self.b)
+    }
+}
+
+pub fn rgb(r: u8, g: u8, b: u8) -> u16 {
+    let r = ((r as f32 / 255f32) * 31f32) as u8;
+    let b = ((b as f32 / 255f32) * 31f32) as u8;
+    let g = ((g as f32 / 255f32) * 63f32) as u8;
+
+    ((r as u16 & 0b11111000) << 8) | ((b as u16 & 0b11111100) << 3) | (g as u16 >> 3)
+}
